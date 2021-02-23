a69f99ab3185c54f8fa20abdccd58e4ab9fcd6a4
수영 김 <ksy9926@naver.com>
2021-02-23 18:27:40 +0900

    test5

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def logout():
 
 # 앱 실행
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug = True)
